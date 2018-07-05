29406a64a1fbd90d649a657cffeb59ec537e77ad
tkosciol <tkosciolek@ucsd.edu>
2018-07-05 15:09:14 -0700

    missed the visualizer sorry

--- q2_american_gut/_visualizer.py
+++ q2_american_gut/_visualizer.py
@@ -38,7 +38,7 @@ def report(output_dir: str,
     _insanity_checker(samples, metadata, table, alpha, pcoa)
 
     # instantiate the reporter
-    rep = Reporter(alpha, pcoa, table, metadata, samples)
+    rep = Reporter(alpha, pcoa, taxonomy, metadata, table, samples)
 
     # instantiate the reporter view
     reporter_view = ReporterView(rep)
